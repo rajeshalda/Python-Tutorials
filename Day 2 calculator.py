af91825894e64a31e4b6b3e4678fe31daf30cf4a
Rajesh Alda <rajeshalda844@gmail.com>
2021-09-13 00:48:14 +0530

    first commit

---
+++ Day 2 calculator.py
@@ -0,0 +1,11 @@
+#2+2 error only worlk on cmd
+#print(hello world) #error
+print("hello world")
+print(2+2) #integer may answer aayega
+print(2+2.5) #floating may answer aayega
+print(int(2+2.5)) #int may answer aayega
+print(2+int(2.5)) #int may answer aayega
+print(2.5+int(2.5)) #floating may answer aayega
+# same this rules follow all the calculation
+print(2/2) #floating may answer aayega
+print(int(2/2)) #int may answer aayega
