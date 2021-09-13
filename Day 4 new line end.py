db27ce15cb7c4b44f6c03843de4f8d4ddc58edc9
Rajesh Alda <rajeshalda844@gmail.com>
2021-09-13 17:07:41 +0530

    first commit

--- Day 4 new line end.py
+++ Day 4 new line end.py
@@ -12,3 +12,9 @@ print("this is new life", end=" and ")
 print("red pill or blue pill")
 
 #end=" " new line character
+
+#if you want to print in same statement without more line
+print("this is new life", "red pill or blue pill" , end=" and ")
+
+#you can write also this , comma means space also
+print("this is new life", "red pill or blue pill")
