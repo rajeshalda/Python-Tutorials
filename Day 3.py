9f83223e5d4df0713ea03977330dc86742811806
Rajesh Alda <rajeshalda844@gmail.com>
2021-09-13 16:28:00 +0530

    first commit

---
+++ Day 3.py
@@ -0,0 +1,12 @@
+#Dont remove this line
+#print("come here")
+#for each line we can use #tag but in large line/eassy etc...
+#then we use triple tag
+
+###this is
+# multiple
+# comment
+#
+#
+# ###
+print("this is new life")
