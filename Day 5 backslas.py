d2fac68e6aa4e44839e5bf575c67689a16ce06b7
Rajesh Alda <rajeshalda844@gmail.com>
2021-09-13 21:01:56 +0530

    first commit

---
+++ Day 5 backslas.py
@@ -0,0 +1,15 @@
+print("RAJESH ALDA")
+#using of \ back slas
+print(" \ rajeshalda") #its f9
+
+print("c:\rajeshalda")
+#result ajeshalda
+
+print("c:\najeshalda")
+#result c:
+#        ajeshalda
+
+#so get a rid of this we use \\ double backslas
+
+print("c:\\rajeshalda")
+#result c:\rajeshalda now its f9
