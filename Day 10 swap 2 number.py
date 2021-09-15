b31480b046e2a521d715b3276394074f55b0e795
Rajesh Alda <rajeshalda844@gmail.com>
2021-09-15 23:31:39 +0530

    first commit

---
+++ Day 10 swap 2 number.py
@@ -0,0 +1,13 @@
+#old way
+# a= 8
+# b= 9
+# temp=a
+# a=b
+# b=temp
+# print(a,b)
+
+#New way
+a=8
+b=9
+a,b=b,a
+print(a,b)
---
+++ Day 9 list and list function.py
@@ -0,0 +1,63 @@
+#shop="vim bar", "sampoo", "dittol", "bread", 25, 60
+#shop=("vim bar", "sampoo", "dittol", "bread", 25, 60)
+#shop=["vim bar", "sampoo", "dittol", "bread", 25, 60]
+#print(shop[2])
+#print(shop[:3])
+#print(shop[::-1])
+
+# result print a list ('vim bar', 'sampoo', 'dittol', 'bread', 25, 60)
+
+#numb =[52, 65, 3, 87, 23, 54]  #remmber some function work on [] this bracket not on () this
+#numb.sort()
+#numb.reverse()
+#print(numb)
+#print(numb[1]) #result 65
+#print(numb[3]) #result 84
+#print(max(numb))
+#print(min(numb))
+
+#numb =[52, 65, 3, 87, 23, 54]
+#numb.append(33) #append matlab end may jod do..
+#numb.append(33)
+#print(numb)    #[52, 65, 3, 87, 23, 54, 33, 33]
+
+#numb=[]
+#numb.append(12)
+#numb.append(55)
+#numb.append(98)
+#print(numb) #result [12, 55, 98]
+
+#numb =[52, 65, 3, 87, 23, 54]
+#numb.insert(2,10)
+#print(numb) #result [52, 65, 10, 3, 87, 23, 54]
+
+#numb =[52, 65, 3, 87, 23, 54]
+#numb.remove(3)
+#print(numb) #result [52, 65, 87, 23, 54]
+
+#numb =[52, 65, 3, 87, 23, 54]
+#numb.pop()
+#print(numb) #result [52, 65, 3, 87, 23] delete the end part of index
+
+#numb =[52, 65, 3, 87, 23, 54]
+#numb[1]=100
+#print(numb)  #result you can change the list of data
+
+#if you want to change the list of data use tuple
+#mutable= can change
+#immutable= can not change #its called tuple
+#tp=(1,2,3)
+#print(tp) #result (1, 2, 3) tuple
+
+#tp=(1,2,3)
+#tp[1]=5
+#print(tp) #result 'tuple' object does not support item assignment #immutable
+
+#tp=(1)
+#print(tp) #result 1 tuple cannot be create
+
+#tp=(1,)
+#print(tp) #result (1,) tuple created
+
+
+
